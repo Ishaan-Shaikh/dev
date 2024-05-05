753bf833bb23fa298b8fe4f2998f335e140750a0
Ishaan Shaikh <144308250+Ishaan-Shaikh@users.noreply.github.com>
2024-05-05 23:43:32 +0530

    dbms practical

---
+++ dbms.sql
@@ -0,0 +1,10 @@
+show databases;
+create database ishaan;
+use database ishaan;
+show tables;
+create table stud(name varchar(20), roll_no int(10));
+insert into stud values ("ishaan", 9);
+alter table stud add id varchar(10);
+desc stud;
+select * from stud;
+insert into stud (roll_no) values (8);
