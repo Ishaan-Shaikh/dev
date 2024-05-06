ee4c2085b7665457b21786f695d2511be74a6bc1
Ishaan Shaikh <144308250+Ishaan-Shaikh@users.noreply.github.com>
2024-05-06 08:18:17 +0530

    Update dbms.sql

--- dbms.sql
+++ dbms.sql
@@ -8,6 +8,7 @@ alter table stud add id varchar(10);
 desc stud;
 select * from stud;
 insert into stud (roll_no) values (8); 
+
 ALTER TABLE stud DROP COLUMN id;
 CREATE TABLE Employees (
     EmployeeID INT PRIMARY KEY,
@@ -16,3 +17,18 @@ CREATE TABLE Employees (
     DepartmentID INT,
     Salary DECIMAL(10, 2)
 );
+CREATE VIEW HighPaidEmployees AS
+SELECT EmployeeID, FirstName, LastName
+FROM Employees
+WHERE Salary > 50000;
+
+CREATE INDEX idx_DepartmentID ON Employees (DepartmentID);
+
+CREATE SEQUENCE EmployeeID_Sequence
+START WITH 100
+INCREMENT BY 1
+MINVALUE 1
+MAXVALUE 1000
+CYCLE;
+
+CREATE SYNONYM Emp FOR Employees;
